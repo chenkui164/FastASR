817b4c7480a324cedf31036640f9a3f5cda665f4
chenkui164 <chenkui164@gmail.com>
2022-07-10 17:43:15 +0800

    更新readme，增加了模型转换代码

--- README.md
+++ README.md
@@ -1,1 +1,93 @@
-hello
+# FastASR
+基于PaddleSpeech所使用的conformer模型，使用C++的高效实现模型推理，在树莓派4B等ARM平台运行也可流畅运行。
+
+## 项目简介
+本项目仅实现了PaddleSpeech [r1.01版本](https://github.com/PaddlePaddle/PaddleSpeech/releases/tag/r1.0.1)中conformer_wenetspeech-zh-16k预训练模型。
+这个预训练模型采用了当下最先进的conformer模型，使用10000+小时的wenetspeech数据集训练得到。
+经过测试它识别效果很好,可以媲美许多商用的ASR软件。
+
+PaddleSpeech是基于python实现的，本身的性能已经很不错了，即使在没有GPU的个人电脑上运行，
+也能满足实时性的要求（如:时长为10s的语音，推理时间小于10s，即可满足实时性）。
+
+
+但是要把PaddleSpeech部署在ARM平台，会遇到两个方面的困难。
+* 不容易安装，需要自己编译一些组件。
+* 执行效率很慢，无法满足实时性的要求。
+
+因此就有这个项目，它由纯C++编写，仅实现了模型的推理过程。
+
+* **语言优势**: 由于C++和Python不同，是编译型语言，编译器会根据编译选项针对不同平台的CPU进行优化，更适合在不同CPU平台上面部署，充分利用CPU的计算资源。
+* **独立**: 实现不依赖于现有的深度学习框架如pytorch、paddle、tensorflow等。
+* **依赖少**: 项目仅使用了两个第三方库libfftw3和libopenblas，并无其他依赖，所以在各个平台的可移植行很好，通用性很强。
+* **效率高**：算法中大量使用指针，减少原有算法中reshape和permute的操作，减少不必要的数据拷贝，从而提升算法性能。
+
+## 快速上手
+### 安装依赖
+
+安装依赖库libfftw3
+```shell
+sudo apt-get install libfftw3-dev libfftw3-single3
+```
+安装依赖库libopenblas
+```shell
+sudo apt-get install libopenblas-dev
+```
+### 编译源码
+下载最新版的源码
+```shell
+git clone https://github.com/chenkui164/FastASR.git
+```
+编译最新版的源码
+```shell
+cd FastASR/
+```
+```shell
+make
+```
+
+### 下载预训练模型
+从PaddleSpeech官网下载预训练模型，如果之前已经在运行过PaddleSpeech，
+则可以不用下载，它已经在目录`~/.paddlespeech/models/conformer_wenetspeech-zh-16k`中。
+```shell
+wget -c https://paddlespeech.bj.bcebos.com/s2t/wenetspeech/asr1_conformer_wenetspeech_ckpt_0.1.1.model.tar.gz
+```
+
+将压缩包解压wenetspeech目录下
+```
+mkdir wenetspeech
+tar -xzvf asr1_conformer_wenetspeech_ckpt_0.1.1.model.tar.gz -C wenetspeech
+```
+将用于Python的模型转换为C++的，这样更方便通过内存映射的方式直接读取参数，加快模型读取速度。
+
+```shell
+./convert.py wenetspeech/exp/conformer/checkpoints/wenetspeech.pdparams
+```
+查看转换后的参数文件wenet_params.bin的md5码，md5码为9cfcf11ee70cb9423528b1f66a87eafd，表示转换正确。
+
+```
+md5sum -b wenet_params.bin
+```
+
+
+同时我也把转换好的wenet_params.bin上传至github，可以直接下载，可能会有些慢。
+``` shell
+wget -c  https://github.com/chenkui164/FastASR/releases/download/V0.01/wenet_params.bin
+```
+
+### 如何使用
+下载用于测试的wav文件
+```shell
+wget -c https://paddlespeech.bj.bcebos.com/PaddleAudio/zh.wav 
+```
+
+执行程序
+```shell
+./fastasr zh.wav
+```
+
+程序输出
+```
+Audio time is 4.996812 s.
+result: "我认为跑步最重要的就是给我带来了身体健康"
+inference time is 0.464350 s.
+```
---
+++ convert.py
@@ -0,0 +1,268 @@
+#!/usr/bin/python
+
+import pickle
+import numpy as np
+import sys
+
+
+def decoder(idx, bb, wfid):
+    key = 'decoder.decoders.{}.self_attn.linear_q.weight'
+    a = bb[key.format(idx)]
+    wfid.write(a.tobytes())
+    key = 'decoder.decoders.{}.self_attn.linear_q.bias'
+    a = bb[key.format(idx)]
+    wfid.write(a.tobytes())
+    key = 'decoder.decoders.{}.self_attn.linear_k.weight'
+    a = bb[key.format(idx)]
+    wfid.write(a.tobytes())
+    key = 'decoder.decoders.{}.self_attn.linear_k.bias'
+    a = bb[key.format(idx)]
+    wfid.write(a.tobytes())
+    key = 'decoder.decoders.{}.self_attn.linear_v.weight'
+    a = bb[key.format(idx)]
+    wfid.write(a.tobytes())
+    key = 'decoder.decoders.{}.self_attn.linear_v.bias'
+    a = bb[key.format(idx)]
+    wfid.write(a.tobytes())
+    key = 'decoder.decoders.{}.self_attn.linear_out.weight'
+    a = bb[key.format(idx)]
+    wfid.write(a.tobytes())
+    key = 'decoder.decoders.{}.self_attn.linear_out.bias'
+    a = bb[key.format(idx)]
+    wfid.write(a.tobytes())
+
+    key = 'decoder.decoders.{}.src_attn.linear_q.weight'
+    a = bb[key.format(idx)]
+    wfid.write(a.tobytes())
+    key = 'decoder.decoders.{}.src_attn.linear_q.bias'
+    a = bb[key.format(idx)]
+    wfid.write(a.tobytes())
+    key = 'decoder.decoders.{}.src_attn.linear_k.weight'
+    a = bb[key.format(idx)]
+    wfid.write(a.tobytes())
+    key = 'decoder.decoders.{}.src_attn.linear_k.bias'
+    a = bb[key.format(idx)]
+    wfid.write(a.tobytes())
+    key = 'decoder.decoders.{}.src_attn.linear_v.weight'
+    a = bb[key.format(idx)]
+    wfid.write(a.tobytes())
+    key = 'decoder.decoders.{}.src_attn.linear_v.bias'
+    a = bb[key.format(idx)]
+    wfid.write(a.tobytes())
+    key = 'decoder.decoders.{}.src_attn.linear_out.weight'
+    a = bb[key.format(idx)]
+    wfid.write(a.tobytes())
+    key = 'decoder.decoders.{}.src_attn.linear_out.bias'
+    a = bb[key.format(idx)]
+    wfid.write(a.tobytes())
+
+    key = 'decoder.decoders.{}.feed_forward.w_1.weight'
+    a = bb[key.format(idx)]
+    wfid.write(a.tobytes())
+    key = 'decoder.decoders.{}.feed_forward.w_1.bias'
+    a = bb[key.format(idx)]
+    wfid.write(a.tobytes())
+    key = 'decoder.decoders.{}.feed_forward.w_2.weight'
+    a = bb[key.format(idx)]
+    wfid.write(a.tobytes())
+    key = 'decoder.decoders.{}.feed_forward.w_2.bias'
+    a = bb[key.format(idx)]
+    wfid.write(a.tobytes())
+
+    key = 'decoder.decoders.{}.norm1.weight'
+    a = bb[key.format(idx)]
+    wfid.write(a.tobytes())
+    key = 'decoder.decoders.{}.norm1.bias'
+    a = bb[key.format(idx)]
+    wfid.write(a.tobytes())
+
+    key = 'decoder.decoders.{}.norm2.weight'
+    a = bb[key.format(idx)]
+    wfid.write(a.tobytes())
+    key = 'decoder.decoders.{}.norm2.bias'
+    a = bb[key.format(idx)]
+    wfid.write(a.tobytes())
+
+    key = 'decoder.decoders.{}.norm3.weight'
+    a = bb[key.format(idx)]
+    wfid.write(a.tobytes())
+    key = 'decoder.decoders.{}.norm3.bias'
+    a = bb[key.format(idx)]
+    wfid.write(a.tobytes())
+
+
+def encoder(idx, bb, wfid):
+    key = 'encoder.encoders.{}.self_attn.linear_q.weight'
+    a = bb[key.format(idx)]
+    wfid.write(a.tobytes())
+    key = 'encoder.encoders.{}.self_attn.linear_q.bias'
+    a = bb[key.format(idx)]
+    wfid.write(a.tobytes())
+    key = 'encoder.encoders.{}.self_attn.linear_k.weight'
+    a = bb[key.format(idx)]
+    wfid.write(a.tobytes())
+    key = 'encoder.encoders.{}.self_attn.linear_k.bias'
+    a = bb[key.format(idx)]
+    wfid.write(a.tobytes())
+    key = 'encoder.encoders.{}.self_attn.linear_v.weight'
+    a = bb[key.format(idx)]
+    wfid.write(a.tobytes())
+    key = 'encoder.encoders.{}.self_attn.linear_v.bias'
+    a = bb[key.format(idx)]
+    wfid.write(a.tobytes())
+    key = 'encoder.encoders.{}.self_attn.linear_out.weight'
+    a = bb[key.format(idx)]
+    wfid.write(a.tobytes())
+    key = 'encoder.encoders.{}.self_attn.linear_out.bias'
+    a = bb[key.format(idx)]
+    wfid.write(a.tobytes())
+    key = 'encoder.encoders.{}.self_attn.linear_pos.weight'
+    a = bb[key.format(idx)]
+    wfid.write(a.tobytes())
+    key = 'encoder.encoders.{}.self_attn.pos_bias_u'
+    a = bb[key.format(idx)]
+    wfid.write(a.tobytes())
+    key = 'encoder.encoders.{}.self_attn.pos_bias_v'
+    a = bb[key.format(idx)]
+    wfid.write(a.tobytes())
+    key = 'encoder.encoders.{}.feed_forward.w_1.weight'
+    a = bb[key.format(idx)]
+    wfid.write(a.tobytes())
+    key = 'encoder.encoders.{}.feed_forward.w_1.bias'
+    a = bb[key.format(idx)]
+    wfid.write(a.tobytes())
+    key = 'encoder.encoders.{}.feed_forward.w_2.weight'
+    a = bb[key.format(idx)]
+    wfid.write(a.tobytes())
+    key = 'encoder.encoders.{}.feed_forward.w_2.bias'
+    a = bb[key.format(idx)]
+    wfid.write(a.tobytes())
+    key = 'encoder.encoders.{}.feed_forward_macaron.w_1.weight'
+    a = bb[key.format(idx)]
+    wfid.write(a.tobytes())
+    key = 'encoder.encoders.{}.feed_forward_macaron.w_1.bias'
+    a = bb[key.format(idx)]
+    wfid.write(a.tobytes())
+    key = 'encoder.encoders.{}.feed_forward_macaron.w_2.weight'
+    a = bb[key.format(idx)]
+    wfid.write(a.tobytes())
+    key = 'encoder.encoders.{}.feed_forward_macaron.w_2.bias'
+    a = bb[key.format(idx)]
+    wfid.write(a.tobytes())
+    key = 'encoder.encoders.{}.conv_module.pointwise_conv1.weight'
+    a = bb[key.format(idx)]
+    wfid.write(a.tobytes())
+    key = 'encoder.encoders.{}.conv_module.pointwise_conv1.bias'
+    a = bb[key.format(idx)]
+    wfid.write(a.tobytes())
+    key = 'encoder.encoders.{}.conv_module.depthwise_conv.weight'
+    a = bb[key.format(idx)]
+    wfid.write(a.tobytes())
+    key = 'encoder.encoders.{}.conv_module.depthwise_conv.bias'
+    a = bb[key.format(idx)]
+    wfid.write(a.tobytes())
+    key = 'encoder.encoders.{}.conv_module.pointwise_conv2.weight'
+    a = bb[key.format(idx)]
+    wfid.write(a.tobytes())
+    key = 'encoder.encoders.{}.conv_module.pointwise_conv2.bias'
+    a = bb[key.format(idx)]
+    wfid.write(a.tobytes())
+    key = 'encoder.encoders.{}.conv_module.norm.weight'
+    a = bb[key.format(idx)]
+    wfid.write(a.tobytes())
+    key = 'encoder.encoders.{}.conv_module.norm.bias'
+    a = bb[key.format(idx)]
+    wfid.write(a.tobytes())
+    key = 'encoder.encoders.{}.norm_ff.weight'
+    a = bb[key.format(idx)]
+    wfid.write(a.tobytes())
+    key = 'encoder.encoders.{}.norm_ff.bias'
+    a = bb[key.format(idx)]
+    wfid.write(a.tobytes())
+    key = 'encoder.encoders.{}.norm_mha.weight'
+    a = bb[key.format(idx)]
+    wfid.write(a.tobytes())
+    key = 'encoder.encoders.{}.norm_mha.bias'
+    a = bb[key.format(idx)]
+    wfid.write(a.tobytes())
+    key = 'encoder.encoders.{}.norm_ff_macaron.weight'
+    a = bb[key.format(idx)]
+    wfid.write(a.tobytes())
+    key = 'encoder.encoders.{}.norm_ff_macaron.bias'
+    a = bb[key.format(idx)]
+    wfid.write(a.tobytes())
+    key = 'encoder.encoders.{}.norm_conv.weight'
+    a = bb[key.format(idx)]
+    wfid.write(a.tobytes())
+    key = 'encoder.encoders.{}.norm_conv.bias'
+    a = bb[key.format(idx)]
+    wfid.write(a.tobytes())
+    key = 'encoder.encoders.{}.norm_final.weight'
+    a = bb[key.format(idx)]
+    wfid.write(a.tobytes())
+    key = 'encoder.encoders.{}.norm_final.bias'
+    a = bb[key.format(idx)]
+    wfid.write(a.tobytes())
+
+
+filename = sys.argv[1]
+fid = open(filename, 'rb')
+
+wfid = open('wenet_params.bin', 'wb')
+
+bb = pickle.load(fid)
+a = bb['encoder.embed.conv.0.weight']
+a = np.transpose(a, (3, 2, 1, 0))
+wfid.write(a.tobytes())
+
+a = bb['encoder.embed.conv.0.bias']
+wfid.write(a.tobytes())
+
+a = bb['encoder.embed.conv.2.weight']
+a = np.transpose(a, (1, 3, 2, 0))
+wfid.write(a.tobytes())
+
+a = bb['encoder.embed.conv.2.bias']
+wfid.write(a.tobytes())
+
+a = bb['encoder.embed.out.0.weight']
+wfid.write(a.tobytes())
+
+a = bb['encoder.embed.out.0.bias']
+wfid.write(a.tobytes())
+
+for i in range(0, 12):
+    encoder(i, bb, wfid)
+
+a = bb['encoder.after_norm.weight']
+wfid.write(a.tobytes())
+a = bb['encoder.after_norm.bias']
+wfid.write(a.tobytes())
+
+
+a = bb['ctc.ctc_lo.weight']
+wfid.write(a.tobytes())
+a = bb['ctc.ctc_lo.bias']
+wfid.write(a.tobytes())
+
+
+a = bb['decoder.embed.0.weight']
+wfid.write(a.tobytes())
+for i in range(0, 6):
+    decoder(i, bb, wfid)
+
+
+a = bb['decoder.after_norm.weight']
+wfid.write(a.tobytes())
+
+a = bb['decoder.after_norm.bias']
+wfid.write(a.tobytes())
+
+a = bb['decoder.output_layer.weight']
+wfid.write(a.tobytes())
+
+a = bb['decoder.output_layer.bias']
+wfid.write(a.tobytes())
+
+wfid.close()
+fid.close()
